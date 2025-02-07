2de7aa5166c2588efbb891dd4f1ad88b0d05c3de
quocvinh0401 <lequocvinh140213@gmail.com>
2025-02-07 21:56:35 +0700

    feat: remove credential

--- main.tf
+++ main.tf
@@ -8,11 +8,6 @@ terraform {
 
 }
 
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
 
 module "vpc" {
   source          = "./modules/vpc"
